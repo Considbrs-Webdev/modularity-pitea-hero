741b4c29fc2073194d8a63daa0bc59e133eecb79
William Lundqvist <william.lundqvist@consid.se>
2026-02-11 09:25:48 +0100

    disabled search butto on empty input

--- source/js/modularity-pitea-hero.js
+++ source/js/modularity-pitea-hero.js
@@ -1,1 +1,22 @@
-console.log('modularity-pitea-hero'); 
+function initPiteaHero() {
+    const heroInput = document.querySelector('.c-pitea-hero__search-input input[name="s"]');
+    const heroSearchButton = document.querySelector('.c-pitea-hero__search-button');
+
+    if (!heroInput || !heroSearchButton) return;
+
+    heroSearchButton.setAttribute('disabled', '');
+    heroInput.addEventListener('input', (e) => {
+        if (heroInput.value.length > 0) {
+            heroSearchButton.removeAttribute('disabled');
+        } else {
+            heroSearchButton.setAttribute('disabled', '');
+        }
+    });
+}
+
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initPiteaHero);
+} else {
+    initPiteaHero();
+}
